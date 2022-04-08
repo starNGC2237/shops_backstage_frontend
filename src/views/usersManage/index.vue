8fd857bb2778e7bd830db369eebd60c9b0c23f84
starNGC2237 <starmars_lu@163.com>
2022-04-08 20:49:15 +0800

    init

--- src/views/usersManage/index.vue
+++ src/views/usersManage/index.vue
@@ -1,12 +1,35 @@
 <template>
-  <h1>用户管理</h1>
+  <div class="userManage">
+    <h1>用户管理</h1>
+    <el-table
+      v-loading="loading"
+      border
+      element-loading-text="加载中..."
+    >
+      <el-table-column
+        prop="userName"
+        label="用户名"
+      />
+      <el-table-column
+        prop="nickName"
+        label="昵称"
+      />
+    </el-table>
+  </div>
 </template>
 <script>
 export default {
-  name: 'UsersManage'
+  name: 'UsersManage',
+  data() {
+    return {
+      loading: false
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
-
+.userManage{
+  padding: 1rem;
+}
 </style>
